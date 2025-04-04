6cacaa0ccc20c78b58728ca91cf23173b84fdf56
Damir Latfullin <itproofan@gmail.com>
2025-04-04 23:51:55 +0500

    Initial commit.

---
+++ src/main/java/kz/aitu/corporationy/CorporationyApplication.java
@@ -0,0 +1,13 @@
+package kz.aitu.corporationy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CorporationyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CorporationyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kz/aitu/corporationy/CorporationyApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.aitu.corporationy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CorporationyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
